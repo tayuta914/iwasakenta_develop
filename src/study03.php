7ef353569558cc9565d0f2a522f3af60024bb1df
hiroto-hash <hiroto5118@icloud.com>
2020-09-09 14:55:54 +0900

    study03修正2

--- src/study03.php
+++ src/study03.php
@@ -15,7 +15,7 @@ td {
   padding: 5px;
 }
   </style>
-  <title>四則演算</title>
+  <title>配列</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -35,7 +35,7 @@ td {
     <?php
     // ココにコーディング
 
-    $fruit =array( "リンゴ", "バナナ", "レモン");
+    $fruit = array("リンゴ", "バナナ", "レモン");
     echo '<pre>';
     print_r ($fruit);
     echo '</pre>';
