b33c775a741057137b28693b88cce04fa4fe2e34
ykobaya14 <job0410kobayashi@gmail.com>
2020-09-14 12:19:09 +0900

    WIPさくらのAPIを叩く

--- src/study09.php
+++ src/study09.php
@@ -3,28 +3,29 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
   <style>
+  div {margin: 10px;}
   h1 {border-bottom: solid;}
   </style>
+  <title>GET・POST2</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
-<body>
-  <h1>研修用問題集</h1>
-  実際に対象のファイルを編集して要件にあったコーディングをしよう!!!!!!
-  <ul>
-    <li><a href="./src/study01.php">四則演算 study01.php</a></li>
-    <li><a href="./src/study02.php">繰り返し study02.php</a></li>
-    <li><a href="./src/study03.php">配列 study03.php</a></li>
-    <li><a href="./src/study04.php">文字列操作 study04.php</a></li>
-    <li><a href="./src/study05.php">カレンダー作成 study05.php</a></li>
-    <li><a href="./src/study06.php">ファイル操作 study06.php</a></li>
-    <li><a href="./src/study07.php">SQL study07.php</a></li>
-    <li><a href="./src/study08.php">データのやりとり study08.php</a></li>
-    <li><a href="./src/study09.php">データのやりとり2 study09.php</a></li>
-  </ul>
-</body>
+  <body>
+    <h1>GET・POST2</h1>
+      <h3>データのやりとり2</h3>
+      <a href="./study08.php">戻る</a>
+      <div>
+        実際のサーバーからデータを取得しなさい。</br>
+        例:</br>
+        http://os3-362-14447.vs.sakura.ne.jp/work_php/contact_list.php</br>
+      </div>
+    <div>
+    <?php
+    // ココにコーディング
+　  ?>
+    </div>
+  </body>
 </html>
